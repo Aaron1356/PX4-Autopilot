ad3b2ad3355a6407a117a9b287ba4d98b0de144e
Aaron Porter <aaron.porter5396@gmail.com>
2026-01-05 09:23:28 -0600

    Revert "Merge branch 'ekf2_multi_optical_flow' of https://github.com/AscendEngineering/GFR-VOXL-PX4 into baro_changes"
    This reverts commit 4de3c4ed986a33db0574f5cd390dd575ed217a20, reversing
    changes made to 087e8b5c2db94e085982c76793d25675fa44f775.
    
    Undo Merge changes

--- src/modules/flight_mode_manager/tasks/ManualPosition/FlightTaskManualPosition.cpp
+++ src/modules/flight_mode_manager/tasks/ManualPosition/FlightTaskManualPosition.cpp
@@ -133,12 +133,7 @@ void FlightTaskManualPosition::_updateSetpoints()
 	FlightTaskManualAltitude::_updateSetpoints(); // needed to get yaw and setpoints in z-direction
 	_acceleration_setpoint.setNaN(); // don't use the horizontal setpoints from FlightTaskAltitude
 
-	if (_param_mpc_vel_only.get()) {
-		_position_setpoint(0) = NAN;
-		_position_setpoint(1) = NAN;
-	} else {
-		_updateXYlock(); // check for position lock
-	}
+	_updateXYlock(); // check for position lock
 
 	_weathervane.update();
 
